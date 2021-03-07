1bbe7a1dae7aaf9b5f79c814f27934d28bd271d5
Gennady Zolotukhin <g.a.zolotukhin@gmail.com>
2021-03-07 17:01:50 +0300

    rename resources/.../categories aand dependecies

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -8,10 +8,10 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-8">
-                    @include('blog.admin.category.includes.item_edit_main_col')
+                    @include('blog.admin.categories.includes.item_edit_main_col')
                 </div>
                 <div class="col-md-3">
-                    @include('blog.admin.category.includes.item_edit_add_col')
+                    @include('blog.admin.categories.includes.item_edit_add_col')
                 </div>
             </div>
         </div>
